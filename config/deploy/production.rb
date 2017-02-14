098388b250064a75ff7a6598710ded45ac12a7f4
NamNV609 <namnv609@gmail.com>
2017-02-14 16:42:46 +0700

    Fix migration in deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,4 @@ set :deploy_via, :remote_cache
 set :deploy_to, "/var/www/html/#{fetch(:application)}"
 set :conditionally_migrate, true
 
-server "127.0.0.1", port: fetch(:port), user: fetch(:user), roles: %w{app web}
+server "127.0.0.1", port: fetch(:port), user: fetch(:user), roles: %w{app web db}
